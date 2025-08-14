e61265ff1bbe0783fc96ba065f52bee73255f069
Harinarayan S <harinarayansubu@gmail.com>
2025-08-14 20:21:46 +0530

    update

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 import { io } from "socket.io-client";
+
 const SOCKET_URL = "https://echo-eyes-backend.onrender.com";
-const socket = io(SOCKET_URL, { transports: ["websocket"] });
+
+const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
+  reconnection: true,         // auto reconnect on disconnect
+  reconnectionAttempts: 5,    // retry up to 5 times
+  reconnectionDelay: 2000     // wait 2s before retry
+});
+
 export default socket;
